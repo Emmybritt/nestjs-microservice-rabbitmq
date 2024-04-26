3bba8439b7e85347813cf84099c2b88e79bdcd8a
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2024-04-26 13:41:34 +0100

    Initial commit

---
+++ apps/travel-booking-platform/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'travel-booking-platform',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/travel-booking-platform'
+};
